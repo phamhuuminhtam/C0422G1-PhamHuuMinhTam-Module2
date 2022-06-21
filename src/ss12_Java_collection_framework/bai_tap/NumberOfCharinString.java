d92ebb81cfb07ddd79c959064e137e69597064f0
phamhuuminhtam <minhtamk17qth@gmail.com>
2022-06-21 21:29:07 +0700

    update

--- src/ss12_Java_collection_framework/bai_tap/NumberOfCharinString.java
+++ src/ss12_Java_collection_framework/bai_tap/NumberOfCharinString.java
@@ -8,16 +8,14 @@ public class NumberOfCharinString {
         System.out.println("Enter a String");
         String string = scanner.nextLine();
         String string1 = string.toLowerCase();
-        int count=1;
-        Map<Character,Integer> charlist = new TreeMap<>();
+        Map<Character, Integer> charlist = new TreeMap<>();
         for (int i = 0; i < string1.length(); i++) {
-            if (string1.charAt(i) ==' '){
+            if (string1.charAt(i) == ' ') {
                 continue;
             }
-            if(!charlist.containsKey(string1.charAt(i))){
-
-                charlist.put(string1.charAt(i),1);
-            }else  charlist.replace(string1.charAt(i),charlist.get(string1.charAt(i))+1);
+            if (!charlist.containsKey(string1.charAt(i))) {
+                charlist.put(string1.charAt(i), 1);
+            } else charlist.replace(string1.charAt(i), charlist.get(string1.charAt(i)) + 1);
         }
         System.out.println(charlist);
 
